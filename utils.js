9cdde515a37f388a763f62b31d33fba612702acc
VictorGabrielMO <victorgabrielengsis@gmail.com>
2024-12-18 22:30:13 -0300

    Commit 9 - Criando Arquivos

---
+++ utils.js
@@ -0,0 +1,10 @@
+// arquivo repositorio.js
+const { readFileSync } = require('fs');
+
+module.exports = function formatarMoeda(valor) {
+    return new Intl.NumberFormat("pt-BR",
+      {
+        style: "currency", currency: "BRL",
+        minimumFractionDigits: 2
+      }).format(valor / 100);
+  }
